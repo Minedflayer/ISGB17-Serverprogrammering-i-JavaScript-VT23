9ef98e60c5be8dbd958d3599a33ad34e6033decf
Victor Tell <viggetell@hotmail.com>
2023-05-13 15:44:52 +0200

    commit

--- W2/client-script.js
+++ W2/client-script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const { urlencoded } = require("express");
+
+let socket = io();
+
+window.addEventListener('load', () => {
+    let buttons = document.querySelectorAll('.birdbutton');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', sendbgk);
+    });
+});
+
+function sendbgk(event) {
+    event.preventDefault();
+
+    let bildnr = event.target.getAttribute('data-birdid');
+
+    socket.emit('bytbild', bildnr);
+
+}
+
+socket.on('nybakgrund', function(data){
+    console.log('Byt bakgrundsbild till: ' + data.bildid);
+    let body = document.querySelector('body');
+    body.setAttribute('style', 
+    'background-image: url("public/images/' + data.bildid + '.jpg"); ');
+});
+
+
+
+
+
+// lyssnare på birdbutton
